d8a3d500dcb7d3ce9e656efcad74a875f1ad2119
Raz-Mo <razmorar@gmail.com>
2023-09-09 05:52:49 -0700

    commit

---
+++ src/hello.java
@@ -0,0 +1,18 @@
+
+public class hello {
+	
+	public static void main(String[] args) {
+		
+		String exclaim = addExcl("hello");
+		System.out.println(exclaim);
+				
+		
+		
+		
+	}
+	
+	public static string addExcl(String s) {
+		return s +!;
+	}
+
+}
